1839cc413784e7b463ef2942b0b61e53031421eb
이정우 <jeongwoo.lee@belltherapeutics.com>
2025-02-03 16:41:01 +0900

    feat:body태그에 스크롤바를 없애는 스타일을 지정했습니다

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko" className={`${pretendard.variable}`}>
-      <body className={`${pretendard.className}`}>{children}</body>
+      <body
+        className={`${pretendard.className} w-[100vw] h-[100vh] bg-blue-600 flex justify-center`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
