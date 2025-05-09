ae23c5830d34b4d66d95d8ca7269aa918b38e535
이정우 <jeongwoo.lee@belltherapeutics.com>
2025-05-09 15:53:52 +0900

    feat: 자동으로 manifest.json 파일 요청하지않도록 하기

--- next.config.ts
+++ next.config.ts
@@ -8,4 +8,7 @@ const nextConfig = {
 export default withPWA({
   ...nextConfig,
   dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: false,
+  skipWaiting: false,
 });
